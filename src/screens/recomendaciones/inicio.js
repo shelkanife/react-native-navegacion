afc5e407656e3d0db08436dc62eb1a696f1ea633
Daniel Car <53944969+yaytz@users.noreply.github.com>
2022-05-13 21:41:49 -0500

    Add files via upload

--- src/screens/recomendaciones/inicio.js
+++ src/screens/recomendaciones/inicio.js
@@ -1,20 +1,63 @@
-import React from 'react'
-import {View,Text,Button} from 'react-native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import React, {useState, useEffect} from 'react';
+import {View, Text, ScrollView, Pressable,StyleSheet} from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Budget from '../../components/Budget';
+import { useIsFocused } from '@react-navigation/native';
+import { getBudgetsList,mapBudgetsToDates } from '../../utils/budgets' 
+import MovementHeader from '../../components/MovementHeader'
+import { styles } from '../../styles/global';
 import RDetalle from './detalle'
 
 const Stack=createNativeStackNavigator()
 
-const RInicio = ({navigation}) => {
-    return(
-        <View style={{flex:1}}>
-            <Text>Pantalla "Recomendaciones"</Text>
-            <Button title='Detalle de recomendacion'
-            onPress={() => navigation.navigate('RecommendationsDetails')}></Button>
 
+const RInicio = ({ navigation }) => {
+    const [budgets, setBudgets] = useState([]);
+    const isFocused = useIsFocused()
+    useEffect(() => {
+        (async() => {
+          let budgets = await getBudgetsList();
+          budgets = mapBudgetsToDates(budgets); // HashMap
+          setBudgets(budgets);
+        })
+        ()
+      }, [isFocused]);
+
+
+    return(
+        <View style={{flex: 1, paddingHorizontal: 5}}>
+        <ScrollView contentContainerStyle={
+        Object.keys(budgets).length
+        ? {flex:1, paddingTop: 20, paddingHorizontal: 20}
+        : {flex:1,justifyContent:'center',alignItems:'center'}}>
+        {Object.keys(budgets).length ? (
+          Object.keys(budgets)
+            .sort((date1, date2) => date1 < date2)
+            .map(date => (
+              <>
+                <MovementHeader key={date} fecha={date} />
+                {budgets[date].map(budget => (
+                  <Budget
+                    concept={budget.concept}
+                    key={budget.id}
+                    budgetData={budget}
+                    navigation={navigation}
+                  />
+                ))}
+              </>
+            ))
+        ) : (
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+            Aún sin movimientos
+          </Text>
+        )}
+      </ScrollView>
+      <ActionButton 
+        buttonColor='#1E63CB'
+        onPress={() => navigation.navigate('RecommendationsDetails')}
+        />
         </View>
-    )
+    );
 }
 
 const SRInicio = () => {
@@ -25,5 +68,14 @@ const SRInicio = () => {
         </Stack.Navigator>
     )
 }
+const localeStyles=StyleSheet.create({
+    mainContainer:{
+      paddingTop: 20,
+      paddingHorizontal: 20,
+      alignItems: 'center',
+      minHeight: '100%',
+    
+    }
+  })
 
 export default SRInicio
